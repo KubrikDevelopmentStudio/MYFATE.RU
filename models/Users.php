0b79ce26c9f53b5b74aaa21955bd1e411b7ee3e7
Ruslan Alfimov <alfimov95@mail.ru>
2016-10-30 23:50:32 +0300

    переделал авторизацию под Yii

--- models/Users.php
+++ models/Users.php
@@ -2,13 +2,14 @@
 /**
  * Created by PhpStorm.
  * User: root
- * Date: 29.10.16
- * Time: 22:23
+ * Date: 30.10.16
+ * Time: 23:11
  */
 
 namespace app\models;
 
-use \yii\db\ActiveRecord;
+use yii\db\ActiveRecord;
+
 
 class Users extends ActiveRecord
 {
