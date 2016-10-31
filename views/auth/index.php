7ea410af81b6a6ac5c401064e5faf72e1701a0d4
Ruslan Alfimov <alfimov95@mail.ru>
2016-11-01 01:15:48 +0300

    Ура, мы на верном пути товарищи!

--- views/auth/index.php
+++ views/auth/index.php
@@ -19,13 +19,13 @@
 		<div class="main-signin__middle">
 			<div class="middle__form">
 				<?php $form = ActiveForm::begin([
-					'id' => 'login-form',
+					'id'     => 'login-form',
 					'method' => 'post',
 					'action' => ['auth/login']
 				]); ?>
 				<?= Html::activeInput('text',         $model, 'userLogin',    ['placeHolder' => 'Логин или E-mail', 'class' => 'login-input-auth']) ?>
-				<?= Html::activeInput('password',     $model, 'password',     ['placeHolder' => 'Пароль', 'class' => 'pass-input-auth'])           ?>
-				<?= Html::activeInput('submit',       $model, 'submitButton', ['value'       => 'ВОЙТИ', 'class' => 'submit-login-auth'])            ?>
+				<?= Html::activeInput('password',     $model, 'password',     ['placeHolder' => 'Пароль', 'class' => 'pass-input-auth'])            ?>
+				<?= Html::activeInput('submit',       $model, 'submitButton', ['value'       => 'ВОЙТИ', 'class' => 'submit-login-auth'])           ?>
 				<br/>
 				<label class="lbl1">
 					<?= Html::activeInput('checkbox', $model, 'rememberMe',   ['class'       => 'checkReg'])         ?>
@@ -48,7 +48,6 @@
               <div class="otherField"><p>Привет всем! Это пробный див. Рад всех видеть.Привет всем!</p></div>
             </div>
           </div>
-
 	<!--ФОРМА РЕГИСТРАЦИИ-->
 	<div class="main-signin1 hiddenMainField">
 		<div class="main-signin__head1">
@@ -57,19 +56,40 @@
 		</div>
 		<div class="main-signin__middle1">
 			<div class="middle__form1">
-				<input type="text" placeholder="Логин" class="loginForm1 loginForm1ForBorder"><br/><hr class="hr" /><br/>
-				<input type="text" class="emailOnSignin1 loginForm1" placeholder="E-mail">
-				<input type="password" placeholder="Пароль" class="passForm1">
-				<input type="password" placeholder="Подтвердите пароль" class="passForm1"><br/><hr class="hr" /><br/>
-				<input type="text" maxlength="2" class="dataDay" placeholder="ДД"><input type="text" maxlength="8" class="dataMonth" placeholder="МЕСЯЦ"><input type="text" maxlength="4" class="dataYear" placeholder="ГОД"><br/><br/>
-				<label><input type="checkbox">&nbspПринять правила сайта</label>
-				<label class="lbl2"><input type="checkbox" class="checkReg" />&nbspОтправлять оповещение на почту (<span><a class="aMore" href="#">подробнее</a></span>)</label>
+                <?php $form = ActiveForm::begin([
+                    'id'     => 'reg-form',
+                    'method' => 'post',
+                    'action' => ['auth/register']
+                ]); ?>
+                <!--Поле Логина пользователя-->
+                <?= Html::activeInput('text',   $model, 'regUserLogin',    ['placeHolder' => 'Введите ваш логин', 'class' => 'loginForm1 loginForm1ForBorder']) ?>
+                <br/><hr class="hr" /><br/>
+                <!--Email пользователя-->
+                <?= Html::activeInput('text',   $model, 'regUserEmail',    ['placeHolder' => 'Введите ваш Email', 'class' => 'loginForm1 emailOnSignin1']) ?>
+                <!--Блок ввода пароля и его повторения-->
+                <?= Html::activeInput('text',   $model, 'regUserPassFirst',    ['placeHolder' => 'Введите ваш пароль', 'class' => 'passForm1']) ?>
+                <?= Html::activeInput('text',   $model, 'regUserPassSecond',    ['placeHolder' => 'Введите ваш пароль ещё раз', 'class' => 'passForm1']) ?>
+                <br/><hr class="hr" /><br/>
+                <!--Блок Даты рождения-->
+                <?= Html::activeInput('text',   $model, 'dateDay',    ['placeHolder' => 'ДД', 'maxlength' => '2', 'id' => 'dD', 'class' => 'dataDay']) ?>
+                <?= Html::activeInput('text',   $model, 'dateDay',    ['placeHolder' => 'МЕСЯЦ', 'id' => 'dM','class' => 'dataMonth']) ?>
+                <?= Html::activeInput('text',   $model, 'dateYear',    ['placeHolder' => 'ГОД', 'maxlength' => '4', 'id' => 'dY','class' => 'dataYear']) ?>
+                <br/><br/>
+                <label>
+                    <?= Html::activeInput('checkBox',   $model, 'agreement',    ['class' => 'agreement']) ?>
+                    Принять правила сайта
+                </label>
+				<label class="lbl2">
+                    <?= Html::activeInput('checkbox',   $model, 'checkReg',    ['class' => 'checkReg']) ?>
+                    Отправлять оповещение на почту(<span><a class="aMore" href="#">подробнее</a></span>)
+                </label>
+
 				<input type="submit" value="Зарегистрироваться" class="submitForm1">
+                <?php ActiveForm::end() ?>
 				<img src="/web/images/arrow.png" class="revertToAut">
 			</div>
 		</div>
 		<div class="main-signin__foot1">
-			<!--<img src="/web/images/arrow.png" class="revertToAut">-->
 		</div>
 	</div>
 	</body>
