18ad77f488f8b3ee1c31bf0cad72930f29b22cbc
Ruslan Alfimov <alfimov95@mail.ru>
2016-10-31 23:05:19 +0300

    новый сотрудник в команде лера

--- models/Users.php
+++ models/Users.php
@@ -13,6 +13,14 @@ use yii\db\ActiveRecord;
 
 class Users extends ActiveRecord
 {
+    /*Поле в таблице IS_ACTIVE*/
+    const IS_ACTIVE_USER   = 'true';
+    const IS_UNACTIVE_USER = 'false';
+
+    public function getAllUsers() {
+        return Users::findAll();
+    }
+
     public static function tableName() {
         return 'USERS';
     }
--- web/js/main.js
+++ web/js/main.js
@@ -5,25 +5,37 @@ $(document).ready(function(){
     setInterval(function() {
         $('.aMore').on('mouseover', function() {
             $('.other').css({'display':'table'});
-            $('.other').animate({'margin-top':'50px', 'opacity':'1'}, {queue:false, duration:500});
+            $('.other').animate({'margin-top':'-40px', 'opacity':'1'}, {queue:false, duration:500});
         });
     }, 500);
 
     setInterval(function() {
         $('.aMore').on('mouseout', function() {
-            $('.other').animate({'margin-top':'350px', 'opacity':'0'}, {queue:false, duration:500});
+            $('.other').animate({'margin-top':'200px', 'opacity':'0'}, {queue:false, duration:500});
         })
     }, 500);
 
     $('.reg').on('click', function() {
         $('.main-signin').animate({'margin-left':'-1000px'}, {queue:false, duration:500});
-        $('.main-signin1').animate({'margin':'-501px 257px', 'left': '475px'}, {queue:false, duration:500}).css({'display':'block'});
+        $('.main-signin1').animate({'margin':'-610px 257px', 'left': '475px'}, {queue:false, duration:500}).css({'display':'block'});
     });
     setInterval(function() {
         $('.revertToAut').on('click', function() {
             $('.main-signin').animate({'margin-left':'725px'}, {queue:false, duration:500});
-            $('.main-signin1').animate({'margin':'-501px 3000px', 'left':'-467px'}, {queue:false, duration:500});
+            $('.main-signin1').animate({'margin':'-610px 3000px', 'left':'-467px'}, {queue:false, duration:500});
         })
     }, 500);
+
+    $('.revertToAut').on('mouseout', function() {
+        $('.revertToAut').stop(true, false);
+        $('.revertToAut').animate({'margin-left':'190px'}, 300);
+    })
+    $('.revertToAut').on('mouseover', function() {
+        for (var i=0; i<1000;i++) {
+            $('.revertToAut').animate({'margin-left':'177px'}, 300);
+            $('.revertToAut').animate({'margin-left':'+183px'}, 300);
+        }
+    })
 });
+
 
